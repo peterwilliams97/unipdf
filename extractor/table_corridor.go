a5490ed6705396b39cff356ce60ad7bbc104d218
Peter Williams <peter.williams@papercut.com>
2020-06-08 20:18:04 +1000

    save

--- extractor/table_corridor.go
+++ extractor/table_corridor.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"math"
 	"sort"
-	"strings"
 
 	"github.com/unidoc/unipdf/v3/common"
 	"github.com/unidoc/unipdf/v3/model"
@@ -61,11 +60,18 @@ func (cells cellList) findCorridors() ([]corridor, []corridor) {
 	var yCorridors []corridor
 	common.Log.Info("findCorridors")
 	for i, cell := range cells {
-		if !strings.Contains(cell.text(), "HONOURS:") {
-			continue
-		}
+		// if !strings.Contains(cell.text(), "BIRTH:") {
+		// 	continue
+		// }
+		// if !strings.Contains(cell.text(), "EDUCATION:") {
+		// 	continue
+		// }
+		// if !strings.Contains(cell.text(), "SUMMARY CURRICULUM VITAE") {
+		// 	continue
+		// }
+
 		corr := cp.corridorY(cell, model.PdfRectangle{Ury: 800, Urx: 600})
-		if len(corr.cells) < 2 {
+		if len(corr.cells) < 3 {
 			continue
 		}
 		yCorridors = append(yCorridors, corr)
@@ -74,7 +80,48 @@ func (cells cellList) findCorridors() ([]corridor, []corridor) {
 			fmt.Printf("%8d: %s\n", j, c)
 		}
 	}
+	sort.Slice(yCorridors, func(i, j int) bool {
+		ci, cj := yCorridors[i].cells, yCorridors[j].cells
+		return len(ci) > len(cj)
+	})
+	uniques := []corridor{yCorridors[0]}
+	for _, corr := range yCorridors[1:] {
+		duplicate := false
+		for _, u := range uniques {
+			if u.contains(corr) {
+				duplicate = true
+				break
+			}
+		}
+		if !duplicate {
+			uniques = append(uniques, corr)
+		}
+	}
+	yCorridors = uniques
+
+	common.Log.Info("findCorridors:Done:yCorridors")
+	for i, corr := range yCorridors {
+		fmt.Printf("%4d: %6.2f %s\n", i, corr.PdfRectangle, corr.cells)
+		for j, c := range corr.cells {
+			fmt.Printf("%8d: %s\n", j, c)
+		}
+	}
 	return yCorridors, nil
+}
+
+func (corr corridor) contains(other corridor) bool {
+	for i, cell := range corr.cells[:len(corr.cells)-len(other.cells)+1] {
+		if other.cells[0] != cell {
+			continue
+		}
+		for j, o := range other.cells {
+			if o != corr.cells[i+j] {
+				return false
+			}
+		}
+		return true
+	}
+	return false
 }
 
 // corridorY returns the longest y corridor  below `cell0`.
@@ -83,36 +130,54 @@ func (cp cellPartition) corridorY(cell0 *textPara, pageSize model.PdfRectangle) 
 	leftCells := cp.leftOf(llx)
 	rightCells := cp.rightOf(urx)
 	common.Log.Info("cell0=%s", cell0)
-	for i, cell := range leftCells.sorted(getUry) {
+	for i, cell := range leftCells.sorted(getUry).reversed() {
 		fmt.Printf("%4d << %s\n", i, cell)
 	}
-	for i, cell := range rightCells.sorted(getUry) {
+	for i, cell := range rightCells.sorted(getUry).reversed() {
 		fmt.Printf("%4d >> %s\n", i, cell)
 	}
 	y := cell0.Ury
-	candidates := cp.below(y).sorted(getUry)
+	candidates := cp.below(y).sorted(getLlx).reversed().sorted(getUry).reversed()
 	// for i, cell := range candidates {
 	// 	fmt.Printf("%4d ** %s\n", i, cell)
 	// }
 	var cells cellList
-	bbox := model.PdfRectangle{Llx: DBL_MAX, Urx: DBL_MIN, Ury: y}
+	bbox := model.PdfRectangle{
+		Llx: pageSize.Llx,
+		Urx: pageSize.Ury,
+		Ury: y}
 
 	for i, cell := range candidates {
 		sameRow := cp.yOverlapped(cell)
-		fmt.Printf("%4d ** %s\n", i, cell)
-		fmt.Printf("%4s ~~ sameRow=%d %s\n", "", len(sameRow), sameRow.sorted(getUrx))
-		llxE := math.Min(bbox.Llx, leftCells.intersection(sameRow).max(getUrx, pageSize.Llx))
-		urxE := math.Max(bbox.Urx, rightCells.intersection(sameRow).min(getLlx, pageSize.Urx))
+		corrCells := sameRow.subtract(leftCells).subtract(rightCells)
+		if len(corrCells) == 0 {
+			continue
+		}
+		if _, ok := corrCells[cell]; !ok {
+			continue
+		}
+
+		immediateLeft := sameRow.intersection(leftCells)
+		immediateRight := sameRow.intersection(rightCells)
+		llxE := immediateLeft.max(getUrx, bbox.Llx)
+		urxE := immediateRight.min(getLlx, bbox.Urx)
 		llx = math.Min(llx, cell.Llx)
 		urx = math.Max(urx, cell.Urx)
-		fmt.Printf("%4s -- %6.2f-%6.2f %6.2f-%6.2f\n", "", llx, urx, llxE, urxE)
-		if llxE > llx || urxE < urx {
+		fmt.Printf("%4d ** %d-%d-%d=%d %s\n", i,
+			len(sameRow), len(leftCells), len(rightCells), len(corrCells), cell)
+		fmt.Printf("%4s ~~   sameRow=%d %s\n", "", len(sameRow), sameRow.sorted(getUrx))
+		fmt.Printf("%4s ~~ corrCells=%d %s\n", "", len(corrCells), corrCells.sorted(getUrx))
+		fmt.Printf("%4s -- inner=%6.2f-%6.2f outer=%6.2f-%6.2f\n", "", llx, urx, llxE, urxE)
+		if !(llxE <= llx && urx <= urxE) {
 			break
 		}
 		bbox.Llx = llxE
 		bbox.Urx = urxE
 		bbox.Lly = cell.Lly
 		cells = append(cells, cell)
+		leftCells = cp.leftOf(llx)
+		rightCells = cp.rightOf(urx)
+		fmt.Printf("%4s -- cells=%d %s\n", "", len(cells), cells)
 	}
 	return corridor{PdfRectangle: bbox, cells: cells}
 }
@@ -123,6 +188,7 @@ type corridor struct {
 }
 type cellPartition struct {
 	baseOrder map[basisT]ordering
+	allCells  cellSet
 }
 
 func (cells cellList) newCellPartition() cellPartition {
@@ -131,14 +197,14 @@ func (cells cellList) newCellPartition() cellPartition {
 	for _, basis := range bases {
 		baseOrder[basis] = cells.newOrdering(basis)
 	}
-	return cellPartition{baseOrder: baseOrder}
+	return cellPartition{baseOrder: baseOrder, allCells: cells.set()}
 }
 
 // yOverlapped returns the cells in that overlap `cell` in the y direction.
 func (cp cellPartition) yOverlapped(cell *textPara) cellSet {
-	above := cp.above(cell.Lly)
-	below := cp.below(cell.Ury)
-	return above.intersection(below)
+	aboveOrEqual := cp.baseOrder[getUry].ge(cell.Lly)
+	belowOrEqual := cp.baseOrder[getLly].le(cell.Ury)
+	return aboveOrEqual.intersection(belowOrEqual)
 }
 
 // below returns a set of cells: cell.ury <= y
@@ -146,7 +212,7 @@ func (cp cellPartition) below(y float64) cellSet {
 	return cp.baseOrder[getUry].le(y)
 }
 
-// above returns a set of cells: cell.lly >= y
+// above returns a set of cells: cell.Lly >= y
 func (cp cellPartition) above(y float64) cellSet {
 	return cp.baseOrder[getLly].ge(y)
 }
@@ -161,31 +227,78 @@ func (cp cellPartition) rightOf(x float64) cellSet {
 	return cp.baseOrder[getLlx].ge(x)
 }
 
-// cellsIntersection returns intersection of s1 and s2
-func (s cellSet) intersection(s2 cellSet) cellSet {
+func (s cellSet) subtract(other cellSet) cellSet {
 	i := cellSet{}
 	for cell := range s {
-		if _, ok := s2[cell]; ok {
+		if _, ok := other[cell]; !ok {
 			i[cell] = true
 		}
 	}
 	return i
 }
 
-func (s cellSet) sorted(basis basisT) cellList {
+// cellsIntersection returns intersection of s1 and s2
+func (s cellSet) intersection(other cellSet) cellSet {
+	i := cellSet{}
+	for cell := range s {
+		if _, ok := other[cell]; ok {
+			i[cell] = true
+		}
+	}
+	return i
+}
+
+func (s cellSet) cellList() cellList {
 	cells := make(cellList, len(s))
 	i := 0
 	for cell := range s {
 		cells[i] = cell
 		i++
 	}
-	cells.sort(basis)
 	return cells
 }
 
+func (s cellSet) sorted(basis basisT) cellList {
+	return s.cellList().sorted(basis)
+}
+
+func (s cellSet) sortedTable() cellList {
+	cells := s.cellList()
+	sort.Slice(cells,
+		func(i, j int) bool {
+			ci, cj := cells[i], cells[j]
+			if ci.Ury != cj.Ury {
+				return ci.Ury > cj.Ury
+			}
+			return ci.Lly < cj.Llx
+		})
+	return cells
+}
+
+func (cells cellList) sorted(basis basisT) cellList {
+	cells.sort(basis)
+	return cells
+}
 func (cells cellList) sort(basis basisT) {
 	sort.SliceStable(cells,
 		func(i, j int) bool { return cells[i].at(basis) < cells[j].at(basis) })
+}
+
+func (cells cellList) reversed() cellList {
+	n := len(cells)
+	rev := make(cellList, n)
+	for i, cell := range cells {
+		rev[n-1-i] = cell
+	}
+	return rev
+}
+
+func (cells cellList) set() cellSet {
+	s := make(cellSet, len(cells))
+	for _, cell := range cells {
+		s[cell] = true
+	}
+	return s
 }
 
 func (s cellSet) min(basis basisT, defVal float64) float64 {
